20748658575a6feb6ccf766f51a4db1d48e8dcd3
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-09-20 18:58:01 +0530

    Added ES6 & ES7 Fn by Maxi bootcamp

--- Basics/Classes.js
+++ Basics/Classes.js
@@ -1,6 +1,6 @@
 class Test {
-    name = "Bang" // Property (variables attached to class called property)
-    call = () => { } // methods (funtion attached to class called methods)
+  name = "Bang"; // Property (variables attached to class called property)
+  call = () => {}; // methods (funtion attached to class called methods)
 }
 
 /**
@@ -11,134 +11,136 @@ class Test {
  */
 
 class Human {
-    // constructor(){
-    //     this.gender= "male"
-    // }
-    gender = "male"
-    printMyGender() {
-        console.log(this.gender)
-    }
+  // ES6 way
+  // constructor(){
+  //     this.gender= "male"
+  // }
+  // ES7 way ( nice and short )
+  gender = "male";
+  printMyGender() {
+    console.log(this.gender);
+  }
 }
 
 class Person extends Human {
-    // constructor(){
-    //     super()
-    //     this.name="shan"
-    //     //this.gender = "can be changed.."
-    // }
-    // super()
+  // we can omit constructor and super in ES7
+  // constructor(){
+  //     super()
+  //     this.name="shan"
+  //     //this.gender = "can be changed.."
+  // }
+  // super()
 
-    name = "hello"
+  name = "hello";
+  gender = "Sigma male"; // change  directly from here
 
-    printMyname() {
-        console.log(this.name, this.gender)
-        //this.gender = "changing from methods"
-    }
+  printMyname() {
+    console.log(this.name, this.gender);
+    //this.gender = "changing from methods"
+  }
 }
 
-const person = new Person()
-person.printMyname()
-person.printMyGender()
-
-
+const person = new Person();
+person.printMyname();
+person.printMyGender();
 
 // *************************************************************
 
 class Vehicle {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
 
-    constructor(name, type) {
-        this.name = name;
-        this.type = type;
-    }
+  getName() {
+    return this.name;
+  }
 
-    getName() {
-        return this.name;
-    }
-
-    getType() {
-        return this.type;
-    }
-
+  getType() {
+    return this.type;
+  }
 }
 
 function getVal(params) {
-    console.log("hello " + params)
+  console.log("hello " + params);
 }
 
 class Car extends Vehicle {
-    constructor(names) {
-        getVal('Bang') // calling fn before super()
-        super(names, 'car');
-        this.myname = names
-    }
+  constructor(names) {
+    getVal("Bang"); // calling fn before super()
+    super(names, "car");
+    this.myname = names;
+  }
 
-    getName() {
-        return 'It is a car: ' + this.name + " (coming from parent) .. " + this.myname + " -> " + super.getName();
-    }
-
+  getName() {
+    return (
+      "It is a car: " +
+      this.name +
+      " (coming from parent) .. " +
+      this.myname +
+      " -> " +
+      super.getName()
+    );
+  }
 }
-let car = new Car('Tesla');
-console.log(car.getName())
-console.log(car.getType())
-
+let car = new Car("Tesla");
+console.log(car.getName());
+console.log(car.getType());
 
 // ***********************************************************
 
 class Animal {
-    constructor(name, breed) {
-        this.name = name
-        this.breed = breed
-    }
-    getName() {
-        return this.name
-    }
-    getBreed() {
-        return this.breed
-    }
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+  getName() {
+    return this.name;
+  }
+  getBreed() {
+    return this.breed;
+  }
 }
 
 class subAnimal extends Animal {
-    constructor(subname,subbreed) {
-        super(subname, subbreed)
-        this.subnameRedifined=subname
-    }
-    getSubname() {
-        return `1 from subclass directly getting name from constructor => ${this.subnameRedifined}`  
-    }
-    getParentAnimalName() {
-        return `2 from subclass name from parent => ${this.name}` 
-    }
-    getAnimalBreed() {
-        return `3 from subclass breed from parent methods => ${super.getBreed()}` 
-    }
-    getAnimalName() {
-        return `4 from subclass name from parent methods => ${super.getName()} + getting 3 => ${this.getAnimalBreed()}`  
-    }
+  constructor(subname, subbreed) {
+    super(subname, subbreed);
+    this.subnameRedifined = subname;
+  }
+  getSubname() {
+    return `1 from subclass directly getting name from constructor => ${this.subnameRedifined}`;
+  }
+  getParentAnimalName() {
+    return `2 from subclass name from parent => ${this.name}`;
+  }
+  getAnimalBreed() {
+    return `3 from subclass breed from parent methods => ${super.getBreed()}`;
+  }
+  getAnimalName() {
+    return `4 from subclass name from parent methods => ${super.getName()} + getting 3 => ${this.getAnimalBreed()}`;
+  }
 }
 
-const animal = new subAnimal('cat', 'animal')
+const animal = new subAnimal("cat", "animal");
 
-function getVal(val){
-    console.log(val)
+function getVal(val) {
+  console.log(val);
 }
 
-getVal(animal.getName())
-getVal(animal.getBreed())
-getVal('********')
-getVal(animal.getSubname())
-getVal(animal.getParentAnimalName())
-getVal(animal.getAnimalBreed())
-getVal(animal.getAnimalName())
-
-
-
+getVal(animal.getName()); // cat
+getVal(animal.getBreed()); // animal
+getVal("********");
+getVal(animal.getSubname()); // ....animal
+getVal(animal.getParentAnimalName()); // ... cat
+getVal(animal.getAnimalBreed()); // ... animal
+getVal(animal.getAnimalName()); // ... cat... ..... animal
 
 // Fn inside Object
 let test = {
-    x: function(){
-        console.log('hey')
-    }
-}
+  x: function () {
+    console.log("hey");
+  },
+};
 
-getVal(test.x)
-test.x()
+getVal(test.x);
+test.x();
