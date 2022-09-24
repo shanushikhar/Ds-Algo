0e3615b0ec7515958d919635d65dbf2bc801759d
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-09-24 18:38:58 +0530

    Added LinkedList basic str

---
+++ Intermediate/Linked List/Example.js
@@ -0,0 +1,20 @@
+// "New" keyword specifically calling the constructor in the class.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+const Linked = new LinkedList(4);
+console.log(Linked);
