72c16d163a96c52a02398482745f0f2cbee63c1d
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-09-27 18:26:07 +0530

    Added Pop fn for linkedlist

--- Linked List/Example.js
+++ Linked List/Example.js
@@ -21,7 +21,7 @@ class LinkedList {
       this.heads = newNode;
       this.tail = newNode;
     } else {
-      console.log("tail..", this.tail);
+      //console.log("tail..", this.tail);
       this.tail.nextxx = newNode;
       this.tail = newNode;
     }
@@ -29,10 +29,39 @@ class LinkedList {
     return this;
   }
   // Pop
+  pop() {
+    //const newNode = new Node(value)
+    if (!this.heads) return "No Node found";
+
+    let temp = this.heads;
+    let pre = this.heads;
+
+    while (temp.nextxx) {
+      pre = temp;
+      temp = temp.nextxx;
+    }
+    this.tail = pre;
+    this.tail.nextxx = null;
+    this.length--;
+    if (this.length === 0) {
+      this.heads = null;
+      this.tail = null;
+    }
+    return temp; //will return the popped item
+    //return this // will return the remaining nodes
+  }
 }
 
-const Linked = new LinkedList(4);
+const Linked = new LinkedList(3);
 //console.log(Linked);
 Linked.push(5);
 Linked.push(7);
+Linked.push(11);
+console.log(Linked);
+
+console.log(Linked.pop());
+console.log(Linked.pop());
+console.log(Linked.pop());
+console.log(Linked.pop());
+console.log(Linked.pop());
 console.log(Linked);
